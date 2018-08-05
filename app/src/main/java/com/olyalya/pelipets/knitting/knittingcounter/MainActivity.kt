c2d091c17d7a893e8c9ca8ae20e58b64cb57dc84
Olga Pelipets <olga.pelipets@macys.com>
2018-08-06 00:22:35 +0300

    first commit

---
+++ app/src/main/java/com/olyalya/pelipets/knitting/knittingcounter/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.olyalya.pelipets.knitting
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_main)
+  }
+}
