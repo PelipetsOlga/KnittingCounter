2b7f9275c5adf3aad1491d56bce9a69790647097
Olga Pelipets <olga.pelipets@macys.com>
2018-08-08 00:15:14 +0300

    add 3 on click listeners

--- app/src/main/java/com/olyalya/pelipets/knitting/MainActivity.kt
+++ app/src/main/java/com/olyalya/pelipets/knitting/MainActivity.kt
@@ -3,6 +3,7 @@ package com.olyalya.pelipets.knitting
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.SeekBar
 import android.widget.SeekBar.OnSeekBarChangeListener
@@ -20,18 +21,22 @@ class MainActivity : AppCompatActivity(), OnSeekBarChangeListener {
   var arrowForward: ImageView? = null
   var arrowBack: ImageView? = null
   var oldProgress = 0
-  var direction = FROM_LEFT_TO_RIGHT
+  var btnSetCounter: Button? = null
+  var btnMinusOne: Button? = null
+  var btnSetZero: Button? = null
+  var btnChangeSide: Button? = null
 
-  private fun setArrowView(value: Direction) {
-    this.direction=value
-    if (value == FROM_LEFT_TO_RIGHT) {
-      arrowForward?.setVisibility(View.VISIBLE)
-      arrowBack?.setVisibility(View.GONE)
-    } else {
-      arrowForward?.setVisibility(View.GONE)
-      arrowBack?.setVisibility(View.VISIBLE)
+  var direction: Direction = FROM_LEFT_TO_RIGHT
+    set(value) {
+      field = value
+      if (value == FROM_LEFT_TO_RIGHT) {
+        arrowForward?.setVisibility(View.VISIBLE)
+        arrowBack?.setVisibility(View.GONE)
+      } else {
+        arrowForward?.setVisibility(View.GONE)
+        arrowBack?.setVisibility(View.VISIBLE)
+      }
     }
-  }
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
@@ -41,7 +46,69 @@ class MainActivity : AppCompatActivity(), OnSeekBarChangeListener {
     tvCounter = findViewById(R.id.tv_counter)
     arrowForward = findViewById(R.id.arrow_forward)
     arrowBack = findViewById(R.id.arrow_back)
+    btnSetCounter = findViewById(R.id.btn_set_counter)
+    btnMinusOne = findViewById(R.id.btn_minus_1)
+    btnSetZero = findViewById(R.id.btn_set_zero)
+    btnChangeSide = findViewById(R.id.btn_change_side)
+
     seekBarView?.setOnSeekBarChangeListener(this);
+    btnSetZero?.setOnClickListener { setCounterToZero() }
+    btnMinusOne?.setOnClickListener { minusOne() }
+    btnChangeSide?.setOnClickListener { changeSide() }
+  }
+
+  fun setCounterToZero() {
+    counter = 0
+    oldProgress = 0
+    currentProgress = 0
+    direction = FROM_LEFT_TO_RIGHT
+    tvCounter?.setText(counter.toString())
+    setOldProgressToSeekBar()
+  }
+
+  fun minusOne() {
+    if (counter > 0) {
+      if (seekBarView?.getProgress() == 0) {
+        counter--
+        oldProgress = 100
+        setOldProgressToSeekBar()
+        direction = FROM_RIGHT_TO_LEFT
+        tvCounter?.setText(counter.toString())
+      } else if (seekBarView?.getProgress() == 100) {
+        counter--
+        oldProgress = 0
+        setOldProgressToSeekBar()
+        direction = FROM_LEFT_TO_RIGHT
+        tvCounter?.setText(counter.toString())
+      } else {
+        if (direction == FROM_LEFT_TO_RIGHT) {
+          oldProgress = 0
+        } else {
+          oldProgress = 100
+        }
+        setOldProgressToSeekBar()
+      }
+    }
+  }
+
+  fun changeSide() {
+    if (seekBarView?.progress == 0) {
+      oldProgress = 100
+      setOldProgressToSeekBar()
+      direction = FROM_RIGHT_TO_LEFT
+    } else if (seekBarView?.progress == 100) {
+      oldProgress = 0
+      setOldProgressToSeekBar()
+      direction = FROM_LEFT_TO_RIGHT
+    } else {
+      oldProgress = 100 - seekBarView?.progress!!
+      setOldProgressToSeekBar()
+      if (direction == FROM_LEFT_TO_RIGHT) {
+        direction = FROM_RIGHT_TO_LEFT
+      } else {
+        direction = FROM_LEFT_TO_RIGHT
+      }
+    }
   }
 
   override fun onResume() {
@@ -50,7 +117,6 @@ class MainActivity : AppCompatActivity(), OnSeekBarChangeListener {
     currentProgress = App.prefs!!.prefCurrentProgress
     oldProgress = App.prefs!!.prefOldProgress
     direction = App.prefs!!.prefDirection
-    setArrowView(direction)
     tvCounter?.setText(counter.toString())
     seekBarView?.setProgress(currentProgress)
   }
@@ -78,8 +144,8 @@ class MainActivity : AppCompatActivity(), OnSeekBarChangeListener {
       if (newProgress!! > oldProgress) {
         if (newProgress > RIGHT_EDGE) {
           oldProgress = 100
-          seekBarView?.setProgress(oldProgress)
-          setArrowView(FROM_RIGHT_TO_LEFT)
+          setOldProgressToSeekBar()
+          direction = FROM_RIGHT_TO_LEFT
           counter++
           tvCounter?.setText(counter.toString())
           currentProgress = 100
@@ -88,15 +154,15 @@ class MainActivity : AppCompatActivity(), OnSeekBarChangeListener {
           currentProgress = oldProgress
         }
       } else {
-        seekBarView?.setProgress(oldProgress)
+        setOldProgressToSeekBar()
         currentProgress = oldProgress
       }
     } else if (direction == FROM_RIGHT_TO_LEFT) {
       if (newProgress!! < oldProgress) {
         if (newProgress < LEFT_EDGE) {
           oldProgress = 0
-          seekBarView?.setProgress(oldProgress)
-          setArrowView(FROM_LEFT_TO_RIGHT)
+          setOldProgressToSeekBar()
+          direction = FROM_LEFT_TO_RIGHT
           counter++
           tvCounter?.setText(counter.toString())
           currentProgress = 0
@@ -105,9 +171,13 @@ class MainActivity : AppCompatActivity(), OnSeekBarChangeListener {
           currentProgress = oldProgress
         }
       } else {
-        seekBarView?.setProgress(oldProgress)
+        setOldProgressToSeekBar()
         currentProgress = oldProgress
       }
     }
+  }
+
+  private fun setOldProgressToSeekBar() {
+    seekBarView?.setProgress(oldProgress)
   }
 }
