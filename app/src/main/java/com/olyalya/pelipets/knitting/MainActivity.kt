20cbf729e5693dc3d9000a289f1f173407545c61
Olga Pelipets <olga.pelipets@macys.com>
2018-08-07 20:00:08 +0300

    old conter bahavior is reproduced

--- app/src/main/java/com/olyalya/pelipets/knitting/MainActivity.kt
+++ app/src/main/java/com/olyalya/pelipets/knitting/MainActivity.kt
@@ -2,22 +2,94 @@ package com.olyalya.pelipets.knitting
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.SeekBar
+import android.widget.SeekBar.OnSeekBarChangeListener
+import android.widget.TextView
+import com.olyalya.pelipets.knitting.Direction.FROM_LEFT_TO_RIGHT
+import com.olyalya.pelipets.knitting.Direction.FROM_RIGHT_TO_LEFT
 
-class MainActivity : AppCompatActivity() {
-  var counter:Int = 0
+class MainActivity : AppCompatActivity(), OnSeekBarChangeListener {
+  val LEFT_EDGE = 20
+  val RIGHT_EDGE = 80
+  var currentProgress = 0
+  var counter: Int = 0
+  var seekBarView: SeekBar? = null
+  var tvCounter: TextView? = null
+  var direction = Direction.FROM_LEFT_TO_RIGHT
+  var oldProgress = 0
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_main)
+
+    seekBarView = findViewById(R.id.seekBar)
+    tvCounter = findViewById(R.id.tv_counter)
+    seekBarView!!!!.setOnSeekBarChangeListener(this);
   }
 
   override fun onResume() {
     super.onResume()
     counter = App.prefs!!.prefCounter
+    currentProgress = App.prefs!!.prefCurrentProgress
+    oldProgress = App.prefs!!.prefOldProgress
+    direction = App.prefs!!.prefDirection
+    tvCounter?.setText(counter.toString())
+    seekBarView?.setProgress(currentProgress)
   }
 
   override fun onPause() {
     App.prefs!!.prefCounter = counter
+    App.prefs!!.prefCurrentProgress = currentProgress
+    App.prefs!!.prefOldProgress = oldProgress
+    App.prefs!!.prefDirection = direction
     super.onPause()
+  }
+
+  override fun onProgressChanged(
+    p0: SeekBar?,
+    p1: Int,
+    p2: Boolean
+  ) {
+  }
+
+  override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+  override fun onStopTrackingTouch(seekBar: SeekBar?) {
+    var newProgress = seekBar?.progress
+    if (direction == FROM_LEFT_TO_RIGHT) {
+      if (newProgress!! > oldProgress) {
+        if (newProgress!! > RIGHT_EDGE) {
+          oldProgress = 100
+          seekBarView?.setProgress(oldProgress)
+          direction = FROM_RIGHT_TO_LEFT
+          counter++
+          tvCounter?.setText(counter.toString())
+          currentProgress = 100
+        } else {
+          oldProgress = newProgress
+          currentProgress = oldProgress
+        }
+      } else {
+        seekBarView?.setProgress(oldProgress)
+        currentProgress = oldProgress
+      }
+    } else if (direction == FROM_RIGHT_TO_LEFT) {
+      if (newProgress!! < oldProgress) {
+        if (newProgress < LEFT_EDGE) {
+          oldProgress = 0
+          seekBarView?.setProgress(oldProgress)
+          direction = FROM_LEFT_TO_RIGHT
+          counter++
+          tvCounter?.setText(counter.toString())
+          currentProgress = 0
+        } else {
+          oldProgress = newProgress
+          currentProgress = oldProgress
+        }
+      } else {
+        seekBarView?.setProgress(oldProgress)
+        currentProgress = oldProgress
+      }
+    }
   }
 }
--- app/src/main/java/com/olyalya/pelipets/knitting/Prefs.kt
+++ app/src/main/java/com/olyalya/pelipets/knitting/Prefs.kt
@@ -2,13 +2,53 @@ package com.olyalya.pelipets.knitting
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.olyalya.pelipets.knitting.Direction.FROM_RIGHT_TO_LEFT
 
-class Prefs (context: Context) {
-  val PREFS_FILENAME = "knitting_counter"
-  val COUNTER_VALUE = "counter_value"
-  val prefs: SharedPreferences = context.getSharedPreferences(PREFS_FILENAME, 0);
+enum class Direction {
+  FROM_LEFT_TO_RIGHT,
+  FROM_RIGHT_TO_LEFT
+}
+
+class Prefs {
+  public constructor(context: Context) {
+    this.context = context
+    this.prefs = context.getSharedPreferences(PREFS_FILENAME, 0);
+    directionMap = HashMap()
+    directionMap.put(DIRECTION_FROM_LEFT_TO_RIGHT, Direction.FROM_LEFT_TO_RIGHT)
+    directionMap.put(DIRECTION_FROM_RIGHT_TO_LEFT, Direction.FROM_RIGHT_TO_LEFT)
+  }
+
+  val DIRECTION_FROM_LEFT_TO_RIGHT = 111
+  val DIRECTION_FROM_RIGHT_TO_LEFT = 112
+  val PREFS_FILENAME = "mysettings"
+  val COUNTER_VALUE = "simplecounter"
+  val CURRENT_PROGRESS = "simpleprogress"
+  val OLD_PROGRESS = "simpleoldprogress"
+  val DIRECTION = "simpledrive"
+
+  var context: Context? = null
+  val directionMap: Map<Int, Direction>
+  var prefs: SharedPreferences? = null;
 
   var prefCounter: Int
-    get() = prefs.getInt(COUNTER_VALUE, 0)
-    set(value) = prefs.edit().putInt(COUNTER_VALUE, value).apply()
+    get() = prefs!!.getInt(COUNTER_VALUE, 0)
+    set(value) = prefs!!.edit().putInt(COUNTER_VALUE, value).apply()
+
+  var prefCurrentProgress: Int
+    get() = prefs!!.getInt(CURRENT_PROGRESS, 0)
+    set(value) = prefs!!.edit().putInt(CURRENT_PROGRESS, value).apply()
+
+  var prefOldProgress: Int
+    get() = prefs!!.getInt(OLD_PROGRESS, 0)
+    set(value) = prefs!!.edit().putInt(OLD_PROGRESS, value).apply()
+
+  var prefDirection: Direction
+    get() = directionMap.get(prefs!!.getInt(DIRECTION, DIRECTION_FROM_LEFT_TO_RIGHT))!!
+    set(value) =
+      if (value == FROM_RIGHT_TO_LEFT) {
+        prefs!!.edit().putInt(DIRECTION, DIRECTION_FROM_RIGHT_TO_LEFT).apply()
+      } else {
+        prefs!!.edit().putInt(DIRECTION, DIRECTION_FROM_LEFT_TO_RIGHT).apply()
+      }
+
 }
